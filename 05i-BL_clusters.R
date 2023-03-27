eb87f4fb616e802a781d19fcd20f18a3ad71d208
Bhavya Singh <singhb39@mcmaster.ca>
2023-03-27 05:43:08 -0400

    updated BL clustering

--- 05i-BL_clusters.R
+++ 05i-BL_clusters.R
@@ -35,134 +35,172 @@ library(ConsensusClusterPlus)
 ################################### LOAD DATA ##################################
 
 load("r_outputs/01-refs.Rdata")
-load("r_outputs/05b-BL_pca_dds.Rdata")
+load("r_outputs/02-BL_filt_counts.Rdata")
 load("r_outputs/01-metadata.Rdata")
 
-##################################### PCA ###################################### 
+############################# FUNCTION SCREE PLOT ##############################
 
-# Just re-plotting here to remind myself what the PCA plots look like
-biplot(BL.herv.pca.obj, 
-       lab = NULL,
-       showLoadings = TRUE,
-       boxedLoadingsNames = TRUE,
-       fillBoxedLoadings = alpha("white", 3/4),
-       pointSize = 3, 
-       encircle = FALSE,
-       sizeLoadingsNames = 4,
-       lengthLoadingsArrowsFactor = 1.5,
-       drawConnectors = TRUE,
-       colby = "ebv_status",
-       shape = "clinical_variant", 
-       shapekey = c("Endemic BL" = 15, "Sporadic BL" = 8),
-       colkey = c("EBV-positive" = wes_palette("Darjeeling1")[2], 
-                  "EBV-negative" = wes_palette("Darjeeling1")[4]),
-       legendPosition = "right")  +
-  theme_cowplot() +
-  theme(aspect.ratio = 1)
+pca_standard <- function(tform, metadata, var) {
+  
+  removeVar <- var
+  pca.obj <- PCAtools::pca(assay(tform), 
+                           metadata=metadata, 
+                           removeVar=removeVar)
+  
+  cat(sprintf('Removed %d pct low variance variables, %d retained\n', 
+              removeVar*100, length(pca.obj$xvars)))
+  
+  varline <- 50
+  varline.x <- min(which(cumsum(pca.obj$variance) >= varline))
+  
+  horn <- PCAtools::parallelPCA(assay(tform), removeVar = removeVar)
+  elbow <- PCAtools::findElbowPoint(pca.obj$variance)
+  
+  screeplot <-PCAtools::screeplot(pca.obj,
+                                  axisLabSize = 6,
+                                  components = getComponents(pca.obj, 1:30),
+                                  title=paste("Retrotranscriptome SCREE",
+                                              metadata$cancer_type[1],
+                                              sep=" "),
+                                  hline=varline, vline=c(varline.x, horn$n, elbow)
+  ) +
+    geom_label(aes(x=varline.x+1, y=50, 
+                   label = paste0(varline, '% var'), vjust = -1)) +
+    geom_label(aes(x = horn$n + 1, y = 50,
+                   label = 'Horn\'s', vjust = -1)) +
+    geom_label(aes(x = elbow + 1, y = 50,
+                   label = 'Elbow method', vjust = -1))
+  
+  
+  cat(sprintf('%d PCs for Elbow method\n', elbow))
+  cat(sprintf('%d PCs for Horn method\n', horn$n))
+  cat(sprintf('%d PCs needed to explain %d percent of variation\n', 
+              varline.x, varline))
+  
+  return(pca.obj)
+}
 
-biplot(BL.g.pca.obj, 
-       lab = NULL,
-       showLoadings = TRUE,
-       boxedLoadingsNames = TRUE,
-       fillBoxedLoadings = alpha("white", 3/4),
-       pointSize = 3, 
-       encircle = FALSE,
-       sizeLoadingsNames = 4,
-       lengthLoadingsArrowsFactor = 1.5,
-       drawConnectors = TRUE,
-       colby = "ebv_status",
-       shape = "clinical_variant", 
-       shapekey = c("Endemic BL" = 15, "Sporadic BL" = 8),
-       colkey = c("EBV-positive" = wes_palette("Darjeeling1")[2], 
-                  "EBV-negative" = wes_palette("Darjeeling1")[4]),
-       legendPosition = "right")  +
-  theme_cowplot() +
-  theme(aspect.ratio = 1)
+################################################################################
+################################################################################
+################################### ALL HERVs ##################################
+################################################################################
+################################################################################
+
+################################ BL DESEQ HERVs ################################
+
+### DESeq2 (HERVs Only)
+
+BL.countdat <- BL.filt.herv
+cat(sprintf('%d variables\n', nrow(BL.countdat)))
+
+stopifnot(all(colnames(BL.countdat) == rownames(BL_metadata)))
+
+BL.dds <- DESeq2::DESeqDataSetFromMatrix(countData = BL.countdat,
+                                            colData = BL_metadata,
+                                            design = ~ 1)
+
+BL.dds <- DESeq2::DESeq(BL.dds, parallel=T)
+BL.tform <- DESeq2::varianceStabilizingTransformation(BL.dds, blind=FALSE)
+
+## PCA
+BL.herv.pca.obj <-
+  pca_standard(tform = BL.tform, 
+               metadata = BL_metadata, 
+               var = 0.7)
+# 4 PCs for Elbow method
+# 15 PCs for Horn method
+# 16 PCs needed to explain 50 percent of variation
 
 ################################## CLUSTERING ##################################
 
+maxK <- 9
 tDat <- assay(BL.tform)[BL.herv.pca.obj$xvars, ]
 cDat <- sweep(tDat, 1, apply(tDat, 1, median, na.rm=T))
 
-BL.ccp.obj <- ConsensusClusterPlus(cDat, maxK=3, reps=1000, pItem=0.8, pFeature=0.8,
-                                title="plots", clusterAlg="km", distance='euclidean',
+ccp.obj <- ConsensusClusterPlus(cDat, maxK=maxK, reps=1000, pItem=0.8, pFeature=0.8,
+                                title="plots/bl_icl", clusterAlg="km", distance='euclidean',
                                 seed=12345, plot="pdf")
-icl <- calcICL(BL.ccp.obj, title="plots", plot='pdf')
+icl <- calcICL(ccp.obj, title="plots/bl_icl", plot='pdf')
 
-stopifnot(all(rownames(BL.herv.pca.obj$metadata) == names(BL.ccp.obj[[2]]$consensusClass)))
+stopifnot(all(rownames(BL.herv.pca.obj$metadata) == names(ccp.obj[[2]]$consensusClass)))
 
-clust.df <- lapply(3, function(k) {
-  factor(paste0('C', BL.ccp.obj[[k]]$consensusClass), levels=paste0('C', 1:k))
+clust.df <- lapply(2:maxK, function(k) {
+  factor(paste0('C', ccp.obj[[k]]$consensusClass), levels=paste0('C', 1:k))
 })%>% bind_cols() %>% data.frame
-colnames(clust.df) <- "clust.retro.k3"
-rownames(clust.df) <- names(BL.ccp.obj[[2]]$consensusClass)
+colnames(clust.df) <- paste0('clust.retro.k', 2:maxK)
+rownames(clust.df) <- names(ccp.obj[[2]]$consensusClass)
 
-BL.herv.pca.obj$metadata <- cbind(BL.herv.pca.obj$metadata, clust.df)
-BL.herv.pca.obj$metadata <- cbind(BL.herv.pca.obj$metadata, BL_metadata$MYC_SV)
-BL.herv.pca.obj$metadata <- cbind(BL.herv.pca.obj$metadata, BL_metadata$MYC_SV_partner)
-BL.herv.pca.obj$metadata <- cbind(BL.herv.pca.obj$metadata, BL_metadata$Total_N_SSM)
-BL.herv.pca.obj$metadata <- cbind(BL.herv.pca.obj$metadata, BL_metadata$subgroup)
-colnames(BL.herv.pca.obj$metadata) <- c("case", "project_id", "submitter_id",
-                                        "sample_type", "tissue_type", "tumor_descriptor",
-                                        "cohort", "clinical_variant", "ebv_status",
-                                        "ebv_genome_type", "gender", "age_at_diagnosis",
-                                        "anatomic_site_classification", "tissue_source_site",
-                                        "cancer_type", "subtype", "clust.retro.k3", 
-                                        "MYC_SV", "MYC_SV_partner", "Total_N_SSM",
-                                        "subgroup")
+BL.herv.pca.obj$metadata <- cbind(
+  BL.herv.pca.obj$metadata, clust.df)
 
 wrap <- function(nclu) {
   columnname <- paste0('clust.retro.k', nclu)
+  colkey <- ccp.obj[[nclu]]$clrs[[3]]
+  names(colkey) <- paste0('C', 1:nclu)
   
   gp <- PCAtools::biplot(BL.herv.pca.obj,
                          colby = columnname,
-                         colkey = c("C1" = wes_palette("Chevalier1")[1], 
-                                    "C2" = wes_palette("Chevalier1")[2],
-                                    "C3" = wes_palette("Chevalier1")[3]),
-                         ellipse = TRUE,
+                         colkey = colkey,
+                         ellipse = FALSE,
                          ellipseConf = 0.9,
-                         shape='ebv_status',
+                         shape = "clinical_variant", 
+                         shapekey = c("Endemic BL" = 15, "Sporadic BL" = 8),
                          hline = 0,
                          vline = 0,
                          legendPosition = 'right',
                          lab=NULL,
-                         title=paste0("Retrotranscriptome CC PCA")
-  ) + theme_cowplot() +
+                         title=paste0("Retrotranscriptome PCA (", columnname, ")")
+  ) + 
+    xlim(-45, 45) + 
+    ylim(-35, 35) + 
     theme(aspect.ratio=1)
   print(gp)
   print(table(BL.herv.pca.obj$metadata[ ,columnname], 
               BL.herv.pca.obj$metadata$ebv_status))
+  print(table(BL.herv.pca.obj$metadata[ ,columnname], 
+              BL.herv.pca.obj$metadata$clinical_variant))
+  print(table(BL.herv.pca.obj$metadata[ ,columnname], 
+              BL.herv.pca.obj$metadata$gender))
+  print(table(BL.herv.pca.obj$metadata[ ,columnname], 
+              BL.herv.pca.obj$metadata$tissue_source_site))
+  print(table(BL.herv.pca.obj$metadata[ ,columnname], 
+              BL.herv.pca.obj$metadata$MYC_SV))
 }
 
-pdf("plots/05i-BL_ccp_PCA.pdf", height=7, width=7)
-wrap(3)
+pdf("plots/05i-bl_cluster_biplot.pdf")
+for(nclu in 2:maxK) {
+  wrap(nclu)
+}
 dev.off()
 
-cplots <- lapply(3, function(nclu) {
+cplots <- lapply(2:maxK, function(nclu) {
   p1 <- factoextra::fviz_cluster(
-    list(data=t(cDat), cluster=BL.ccp.obj[[nclu]]$consensusClass),
-    ellipse.type="norm", ellipse.level=0.9,
+    list(data=t(cDat), cluster=ccp.obj[[nclu]]$consensusClass),
+    # ellipse.type="norm", 
+    # ellipse.level=0.9,
     geom="point",
     palette = "npg"
   ) + theme_cowplot()
-  sil <- cluster::silhouette(BL.ccp.obj[[nclu]]$consensusClass, dist(t(cDat), method = "euclidean"))
+  sil <- cluster::silhouette(ccp.obj[[nclu]]$consensusClass, dist(t(cDat), method = "euclidean"))
   p2 <- factoextra::fviz_silhouette(
-    cluster::silhouette(BL.ccp.obj[[nclu]]$consensusClass, dist(t(cDat), method = "euclidean")),
+    cluster::silhouette(ccp.obj[[nclu]]$consensusClass, dist(t(cDat), method = "euclidean")),
     palette = "npg"
   )
   return(list(p1,p2))
 })
 cplots <- unlist(cplots, recursive = FALSE)
 
-pdf("plots/05i-BL_clusters.pdf", height=5, width=10)
-gridExtra::grid.arrange(grobs = cplots, ncol=2)
+pdf("plots/05i-bl_clusters_analysis.pdf", height=11.5, width=11.5)
+gridExtra::marrangeGrob(grobs = cplots, layout_matrix=matrix(1:8,4,2,T))
 dev.off()
 
-sil.score <- sapply(3, function(k) {
-  mean(cluster::silhouette(BL.ccp.obj[[k]]$consensusClass, 
-                           dist(t(cDat),
-                                method = "euclidean"))[,3])
+sil.score <- sapply(2:maxK, function(k) {
+  mean(cluster::silhouette(ccp.obj[[k]]$consensusClass, dist(t(cDat), method = "euclidean"))[,3])
 })
+ggplot(data.frame(x=2:maxK, y=sil.score)) + geom_point(aes(x,y)) + geom_line(aes(x,y)) + 
+  xlab("k (num clusters)") + ylab('average silhouette score') + theme_cowplot()
+ggsave("plots/05i_bl_clusters_sil_score.pdf")
+
 
 pdf("plots/05i-BL_eigen.pdf", height=8, width=10)
 PCAtools::eigencorplot(BL.herv.pca.obj,
@@ -180,6 +218,6 @@ BL_metadata$clust.retro.k3 <- clust.df
 
 ################################## SAVE FILES ##################################
 
-save(BL.herv.pca.obj, BL.ccp.obj, tDat, cDat, clust.df, BL_metadata,
+save(BL.herv.pca.obj, ccp.obj, tDat, cDat, clust.df, BL_metadata,
      file="r_outputs/05i-BL_pca_ccp_clusters_metadata.Rdata")
 
